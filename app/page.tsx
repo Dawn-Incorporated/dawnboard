7dd3f5c95d3c69a1d4b9f89a64a71582aa10ecab
matisbyar <matis.byar.06@icloud.com>
2024-02-26 23:21:56 +0100

    Editor with tabs

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,59 @@ import React, { useState } from "react";
 import styles from "./page.module.scss";
 import { jsonToYaml, yamlToJson } from "@/lib/yaml";
 
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     const [yaml, setYaml] = useState<string>("");
     const [json, setJson] = useState<string>("");
 
     return (
         <main className={ styles.main }>
-            <div>
-                <h1>YAML to JSON</h1>
-                <textarea value={ yaml } onChange={ (e) => setYaml(e.target.value) }/>
-                <button onClick={ () => setJson(yamlToJson(yaml)) }>make</button>
-            </div>
-            <div>
-                <h1>JSON to YAML</h1>
-                <textarea value={ json } onChange={ (e) => setJson(e.target.value) }/>
-                <button onClick={ () => setYaml(jsonToYaml(json)) }>make</button>
-            </div>
+            <Tabs defaultValue="yaml" className="w-[400px]">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="yaml" onClick={ () => setYaml(jsonToYaml(json)) }>Yaml</TabsTrigger>
+                    <TabsTrigger value="json" onClick={ () => setJson(yamlToJson(yaml)) }>JSON</TabsTrigger>
+                </TabsList>
+                <TabsContent value="yaml">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>YAML Editor</CardTitle>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="space-y-1">
+                                <Textarea value={ yaml } onChange={ (e) => setYaml(e.target.value) }
+                                          rows={ 20 }/>
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button className="flex-auto" onClick={ () => {
+                                setYaml(jsonToYaml(yamlToJson(yaml)));
+                            } }>Format Yaml</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="json">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>JSON Editor</CardTitle>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="space-y-1">
+                                <Textarea value={ json } onChange={ (e) => setJson(e.target.value) }
+                                          rows={ 20 }/>
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button className="flex-auto" onClick={ () => {
+                                setJson(JSON.stringify(JSON.parse(json), null, 2));
+                            } }>Format JSON</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
         </main>
     );
 }
--- lib/yaml.ts
+++ lib/yaml.ts
@@ -2,6 +2,8 @@ import yaml from 'js-yaml';
 
 export const yamlToJson = (doc: string): string => {
     try {
+        if (doc === "") return "";
+
         const jsonObj = yaml.load(doc);
         return JSON.stringify(jsonObj, null, 2);
     } catch (e) {
@@ -12,6 +14,8 @@ export const yamlToJson = (doc: string): string => {
 
 export const jsonToYaml = (doc: string): string => {
     try {
+        if (doc === "") return "";
+
         const jsonObj = JSON.parse(doc);
         return yaml.dump(jsonObj);
     } catch (e) {
